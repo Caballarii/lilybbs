7439393b68693435129e27b64114f6fc7ef63888
Caballarii <xunzongmiying@126.com>
2018-06-28 18:09:19 +0800

    Board Section List
    Board Section List

--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -1,13 +1,29 @@
 import React,{Component} from 'react';
-import {View,Text} from 'react-native';
+import {View,Text,SectionList} from 'react-native';
+
+import {ListItem,SearchBar} from 'react-native-elements';
+
+import boards from '../utils/Board';
 
 class BoardList extends Component{
+
+    toSearchBoard=(text)=>{
+        console.log(text);
+    }
+
     render(){
         return (
             <View>
-                <Text>
-                    settings
-                </Text>
+                <SearchBar
+                    onChangeText={this.toSearchBoard}
+                    onClear={this.toSearchBoard}
+                    placeholder='输入你想去的版面' />
+                <SectionList
+                    renderItem={({item}) => <ListItem containerStyle={{backgroundColor:"white"}} title={item.name+"("+item.desc+")"}/>}
+                    renderSectionHeader={({section}) => <View style={{height:30,justifyContent:"center",paddingLeft:20}}><Text style={{fontSize:18}}>{section.key}</Text></View>}
+                    sections={boards}
+                    keyExtractor={(item, index) => item + index}
+                />
             </View>
         );
     }
