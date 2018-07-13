426ae6d681b0f70f323994dfc5d359654cb5e14c
Caballarii <xunzongmiying@126.com>
2018-07-13 23:27:45 +0800

    daily commit

--- src/components/Post.js
+++ src/components/Post.js
@@ -62,6 +62,7 @@ class Post extends Component{
                                         );
                                     }
                                 })}
+                                
                             </CommonText>);
                         }
                         else{
