f56eee6edbbe0ec5902755f4d138df39e252501e
Caballarii <xunzongmiying@126.com>
2018-08-08 23:58:57 +0800

    daily commit

--- App.js
+++ App.js
@@ -25,21 +25,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-// import {FetchUtil} from './src/utils/FetchUtil';
-// import Cheerio from "cheerio-without-node-native";
-// FetchUtil('bbsboa?sec=11').then(data=>{
-//   let $=Cheerio.load(data);
-//   let result="";
-//   $("tr").each(function(i, e) {
-//     if(i==0)return;
-//     let item=$(this);
-//     let name=item.children("td").eq(2).children("a").html();
-
-//     let descUni=item.children("td").eq(5).children("a").html();
-//     let desc=unescape(descUni.replace(/&#x/g,'%u').replace(/;/g,'')).substring(3);
-
-//     result+=("insertBoard('"+name+"','"+desc+"');\n");
-//   });
-//   console.log(result);
-// })
